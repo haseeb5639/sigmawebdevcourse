6187e349f3315d0d409a8f67a17566d9aad045a3
haseeb5639 <75010951+haseeb5639@users.noreply.github.com>
2024-07-22 17:12:42 +0500

    JavaScript Variables, Data Types & Objects | Sigma Web Development Course - Tutorial #55

---
+++ Lecture 55/script.js
@@ -0,0 +1,41 @@
+console.log("Hey this is tutorial 55")
+
+var a = 5;
+var b = 6;
+var c = "Haseeb";
+var _a = 11;
+// var 45_b = 10;
+
+let d = 66;
+
+console.log(a + b + 8);
+console.log(typeof a, typeof b, typeof c);
+
+{
+    let d = 44;
+    console.log(d);
+}
+
+console.log(d);
+// Constant can not be changed
+const a1 = 6;
+
+
+let x = "Harry bhai";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+console.log(x, y, z, p, q, r);
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r);
+
+let o = {
+    "name" : "Harry",
+    "Job Code" : 56 ,
+    "is_Handsome" : true
+}
+o.salary = "100crores"
+console.log(o);
+o.salary = "500 crores"
+console.log(o);
