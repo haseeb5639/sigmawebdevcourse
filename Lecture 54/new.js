b8bcadc67506f655cf5a89e54d5d9df24551a344
haseeb5639 <75010951+haseeb5639@users.noreply.github.com>
2024-07-22 16:33:26 +0500

    Introduction to JavaScript & Installing Node.js | Sigma Web Development Course - Tutorial #54

---
+++ Lecture 54/new.js
@@ -0,0 +1,4 @@
+console.log("Hello World...")
+console.log("Code is running...")
+console.log("Code was running...")
+console.log("Code will be running...")
---
+++ Lecture 54/script.js
@@ -0,0 +1,20 @@
+// alert("Hello World");
+
+console.log("Code is running...")
+console.log("Code was running...")
+console.log("Code will be running...")
+
+
+// var a = prompt("Enter your number")
+// var isTrue = confirm("Are you sure you want to leave this page")
+// if(isTrue) {
+//     console.log("Leaving Page")
+// }
+// else {
+//     console.log("Staying on the page")
+// }
+// console.log(a)
+
+document.title = "New Title"
+
+document.body.style.backgroundColor = "red"
