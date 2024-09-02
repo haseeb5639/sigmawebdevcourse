b884a4225b0c9559a337de9154acc6d2a31def6f
haseeb5639 <75010951+haseeb5639@users.noreply.github.com>
2024-09-02 21:31:29 +0500

    Introduction to React & Why use React? | Sigma Web Development Course - Tutorial #105

---
+++ Lecture 105/with-react/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <div>
+      This is a footer updated
+    </div>
+  )
+}
+
+export default footer
---
+++ Lecture 105/with-react/src/components/main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Footer from './Footer'
+
+const main = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default main
